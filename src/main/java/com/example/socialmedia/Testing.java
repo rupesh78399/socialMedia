6b37557c048d275c9d4c2c79212d127fabaee627
Rupesh <rupeshp78399@gmail.com>
2026-05-29 22:24:06 +0530

    Initial commit

--- src/main/java/com/example/socialmedia/Testing.java
+++ src/main/java/com/example/socialmedia/Testing.java
@@ -5,8 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class Testing {
 
     @GetMapping("/")
-    public String home(){
-        return "Backend Running";
+    public String home() {
+        return "Backend Running BY Rupesh";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+    @GetMapping("/version")
+    public String version() {
+        return "v1.0";
     }
 }
 
