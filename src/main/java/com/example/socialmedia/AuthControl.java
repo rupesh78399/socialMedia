ba6dee29d83fe7e39a738675823dd1bf7bb4ae09
Rupesh <rupeshp78399@gmail.com>
2026-06-03 23:56:05 +0530

    Disable failing test

--- src/main/java/com/example/socialmedia/AuthControl.java
+++ src/main/java/com/example/socialmedia/AuthControl.java
@@ -2,10 +2,7 @@ package com.example.socialmedia;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -38,5 +35,10 @@ public class AuthControl {
         }
 
         return ResponseEntity.ok("Login successful");
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Auth Controller Working";
     }
 }
